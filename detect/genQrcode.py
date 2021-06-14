a6b9ef9ec52b683922b584ff7e1796349feea4c7
yenhsiu <yan00098@gmail.com>
2021-06-14 17:20:40 +0800

    QRcode pic

--- detect/genQrcode.py
+++ detect/genQrcode.py
@@ -1,12 +1,33 @@
 import qrcode
-import numpy as np
-import pyzbar.pyzbar as zbar
-import cv2
-import matplotlib.pyplot as plt
+from PIL import Image
 
-qr = qrcode.QRCode(version = 15, error_correction = qrcode.constants.ERROR_CORRECT_H, box_size = 3,border = 4) #1~40的整數
-qr.add_data('hello') # QRCode資訊
+qr = qrcode.QRCode(version = 15, error_correction = qrcode.constants.ERROR_CORRECT_H, box_size = 3,border = 4)
+qr.add_data('lion king')
 qr.make(fit = True)
 
 img = qr.make_image()
-img.save("./QRcodehello.png") # 儲存圖片
+img = img.convert('RGBA')
+# 設定色彩
+# img = qr.make_image(fill_color="blue")
+
+# 讀取客製化圖片以嵌入QR code中
+icon = Image.open('/content/lionKing.jpeg')
+img_w, img_h = img.size
+factor = 4
+size_w = int(img_w/factor)
+size_h = int(img_h/factor)
+
+icon_w, icon_h = icon.size
+if icon_w > size_w:
+	icon_w = size_w
+if icon_h > size_h:
+	icon_h = size_h
+
+icon = icon.resize((icon_w, icon_h), Image.NEAREST)
+
+w = int((img_w - icon_w)/2)
+h = int((img_h - icon_h)/2)
+icon = icon.convert('RGBA')
+img.paste(icon, (w,h), icon)
+
+img.save("logoQR1.png")
